1cd732d1da60166674d96e153c40c7158ac15c36
lingzhee <40168641+lingzhee@users.noreply.github.com>
2021-06-10 15:13:45 +0800

    Update TalkApplication.java

--- src/main/java/com/windy/talk/TalkApplication.java
+++ src/main/java/com/windy/talk/TalkApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TalkApplication {
 
-	public static void main(String[] args) {
+	public static void mainHandle(String[] args) {
 		SpringApplication.run(TalkApplication.class, args);
 	}
 
